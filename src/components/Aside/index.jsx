766f304e72da637b2e1302c6865762926bb23643
Sandra Bastos <sandra.bastos@alura.com.br>
2025-05-29 17:36:18 -0300

    initial commit

---
+++ src/components/Aside/index.jsx
@@ -0,0 +1,7 @@
+import styles from './aside.modules.css'
+
+export const Aside = () => {
+    return (<aside className={styles.aside}>
+        <img src="/logo.png" alt="Logo da Code Connect" />
+    </aside>)
+}
